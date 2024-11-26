444a71137fa757d375e0c299b4eeafe5afcb1768
Map987 <99967856+Map987@users.noreply.github.com>
2024-11-27 04:56:43 +0800

    Update live_recorder.py

--- live_recorder.py
+++ live_recorder.py
@@ -205,7 +205,7 @@ class Bilibili(LiveRecoder):
     async def run(self):
         url = f'https://live.bilibili.com/{self.id}'
         if url not in recording:
-	    headers = {
+            headers = {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
                 'Referer': 'https://www.bilibili.com'  # 添加 Referer 字段
 	    }
